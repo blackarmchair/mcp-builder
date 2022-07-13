5cfd87170c04bc96cd6f9aaeaf49b20c82fad64c
Taj Shahrani <tajshahrani@gmail.com>
2022-07-13 13:25:16 -0500

    heimdall and skurge

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const App = () => {
 							<RosterProvider>
 								<Switch>{paths.map((path) => path.route)}</Switch>
 							</RosterProvider>
-							<Version version="1.0.1" />
+							<Version version="1.0.2" />
 						</RouterProvider>
 					</SearchProvider>
 				</ToastProvider>
