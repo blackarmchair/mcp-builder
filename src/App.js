a806886b1d82af00dbd56816b7348ff6b0c84aae
Taj Shahrani <tajshahrani@gmail.com>
2022-10-23 22:53:18 -0500

    added new tt cards and characters

--- src/App.js
+++ src/App.js
@@ -1,72 +1,72 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Alert, Button } from '@mui/material';
+import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Alert, Button } from "@mui/material";
 
-import { SearchProvider } from './contexts/SearchContext';
-import { RosterProvider } from './contexts/RosterContext';
-import { ToastProvider } from './contexts/ToastContext';
-import { RouterProvider } from './contexts/RouterContext';
+import { SearchProvider } from "./contexts/SearchContext";
+import { RosterProvider } from "./contexts/RosterContext";
+import { ToastProvider } from "./contexts/ToastContext";
+import { RouterProvider } from "./contexts/RouterContext";
 
-import paths from './paths';
+import paths from "./paths";
 
-import TopBar from './components/Sidebar';
-import Background from './components/Background';
-import Version from './components/Version';
+import TopBar from "./components/Sidebar";
+import Background from "./components/Background";
+import Version from "./components/Version";
 
 const App = () => {
-	const [appHasUpdates, setAppHasUpdates] = React.useState(false);
-	const [serviceWorkerRegistration, setServiceWorkerRegistration] =
-		React.useState();
+  const [appHasUpdates, setAppHasUpdates] = React.useState(false);
+  const [serviceWorkerRegistration, setServiceWorkerRegistration] =
+    React.useState();
 
-	React.useEffect(() => {
-		window.addEventListener('applicationUpdate', (e) => {
-			setAppHasUpdates(true);
-			setServiceWorkerRegistration(e.detail.reg);
-		});
-		window.addEventListener('load', function () {
-			setTimeout(() => {
-				window.scrollTo(0, 1);
-			}, 0);
-		});
-	}, []);
+  React.useEffect(() => {
+    window.addEventListener("applicationUpdate", (e) => {
+      setAppHasUpdates(true);
+      setServiceWorkerRegistration(e.detail.reg);
+    });
+    window.addEventListener("load", function () {
+      setTimeout(() => {
+        window.scrollTo(0, 1);
+      }, 0);
+    });
+  }, []);
 
-	const handleAppUpgrade = () => {
-		setAppHasUpdates(false);
-		serviceWorkerRegistration
-			.unregister()
-			.then(() => window.location.reload(true)); // `true` argument tells browser to hard reload
-	};
+  const handleAppUpgrade = () => {
+    setAppHasUpdates(false);
+    serviceWorkerRegistration
+      .unregister()
+      .then(() => window.location.reload(true)); // `true` argument tells browser to hard reload
+  };
 
-	return (
-		<>
-			{appHasUpdates && (
-				<Alert
-					severity="success"
-					action={
-						<Button size="small" onClick={handleAppUpgrade}>
-							Upgrade
-						</Button>
-					}
-				>
-					A new version of FridayAI is available.
-				</Alert>
-			)}
-			<Router>
-				<ToastProvider>
-					<SearchProvider>
-						<RouterProvider>
-							<Background />
-							<TopBar />
-							<RosterProvider>
-								<Switch>{paths.map((path) => path.route)}</Switch>
-							</RosterProvider>
-							<Version version="1.0.5" />
-						</RouterProvider>
-					</SearchProvider>
-				</ToastProvider>
-			</Router>
-		</>
-	);
+  return (
+    <>
+      {appHasUpdates && (
+        <Alert
+          severity="success"
+          action={
+            <Button size="small" onClick={handleAppUpgrade}>
+              Upgrade
+            </Button>
+          }
+        >
+          A new version of FridayAI is available.
+        </Alert>
+      )}
+      <Router>
+        <ToastProvider>
+          <SearchProvider>
+            <RouterProvider>
+              <Background />
+              <TopBar />
+              <RosterProvider>
+                <Switch>{paths.map((path) => path.route)}</Switch>
+              </RosterProvider>
+              <Version version="1.0.6" />
+            </RouterProvider>
+          </SearchProvider>
+        </ToastProvider>
+      </Router>
+    </>
+  );
 };
 
 export default App;
